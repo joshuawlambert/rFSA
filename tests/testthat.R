a9500d268dc9c60a8c52b08ea1956704cdc87c2d
Liyu Gong <gongliyu@gmail.com>
2017-07-07 17:01:36 -0400

    add a test case

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(rFSA2)
+
+test_check("rFSA2")
---
+++ tests/testthat/test-FSA.R
@@ -0,0 +1,19 @@
+context("FSA")
+
+## TODO: Rename context
+## TODO: Add more tests
+
+set.seed(1000)
+numrs <- 10 #number of start point
+N <- 100 #number of obs
+P <- 100 #number of variables
+data <- data.frame(matrix(rnorm(N*(P+1)), nrow = N, ncol = P+1))
+set.seed(1000)
+sln <- FSA(formula = "X101~1", data = data, cores = 1, m = 2,
+           interactions = F, criterion = AIC, minmax = "min",
+           numrs = numrs,usehist=T)
+
+test_that("multiplication works", {
+    expect_equivalent(as.character(sln$Variable.Names), "X7, X83")
+    expect_equal(sln$Criterion, 266.028, tolerance=1e-2)
+})
