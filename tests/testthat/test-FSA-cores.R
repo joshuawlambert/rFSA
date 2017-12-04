9142257fc2658fe58199a612f10085e78ee0d76d
Liyu Gong <gongliyu@gmail.com>
2017-12-04 14:15:09 -0500

    tweak test cases for windows

--- tests/testthat/test-FSA-cores.R
+++ tests/testthat/test-FSA-cores.R
@@ -18,10 +18,20 @@ for (cores in c(1, parallel::detectCores())) {
     skip_on_cran()
 
     set.seed(1000)
-    res <- FSA(
-    formula = "X101~1", data = data, cores = cores, m = 2,
-    interactions = F, criterion = AIC, minmax = "min",
-    numrs = numrs, return.models=FALSE)
+    if (.Platform$OS.type == "windows" & cores > 1) {
+      expect_warning(
+        res <- FSA(
+          formula = "X101~1", data = data, cores = cores, m = 2,
+          interactions = F, criterion = AIC, minmax = "min",
+          numrs = numrs, return.models=FALSE)
+      )
+    } else {
+      res <- FSA(
+        formula = "X101~1", data = data, cores = cores, m = 2,
+        interactions = F, criterion = AIC, minmax = "min",
+        numrs = numrs, return.models=FALSE)
+    }
+    
 
     expect_equivalent(as.formula(res$table$formula), as.formula("X101~X7+X83"))
     expect_equal(res$table$criterion,266.028,
@@ -29,12 +39,20 @@ for (cores in c(1, parallel::detectCores())) {
     expect_false("swapped.to.model" %in% names(res$solutions))
     expect_false("checked.model" %in% names(res$solutions))
 
-
     set.seed(1000)
-    res <- FSA(
-      formula = "X101~1", data = data, cores = cores, m = 2,
-      interactions = F, criterion = AIC, minmax = "min",
-      numrs = numrs, return.models=TRUE)
+    if (.Platform$OS.type == "windows" & cores > 1) {
+      expect_warning(
+        res <- FSA(
+          formula = "X101~1", data = data, cores = cores, m = 2,
+          interactions = F, criterion = AIC, minmax = "min",
+          numrs = numrs, return.models=TRUE)
+      )
+    } else {
+      res <- FSA(
+        formula = "X101~1", data = data, cores = cores, m = 2,
+        interactions = F, criterion = AIC, minmax = "min",
+        numrs = numrs, return.models=TRUE)
+    }
 
     expect_equivalent(as.formula(res$table$formula), as.formula("X101~X7+X83"))
     expect_equal(res$table$criterion,266.028,
@@ -49,19 +67,36 @@ for (cores in c(1, parallel::detectCores())) {
     skip_on_cran()
     
     set.seed(1000)
-    res <- FSA(fitfunc=lm,formula="mpg~hp+wt", data=mtcars, fixvar="hp",
-               quad=F, m=2, numrs=10, cores=cores, criterion = r.squared, minmax="max",
-               return.models=FALSE)
+
+    if (.Platform$OS.type == "windows" & cores > 1) {
+      expect_warning(
+        res <- FSA(fitfunc=lm,formula="mpg~hp+wt", data=mtcars, fixvar="hp",
+                   quad=F, m=2, numrs=10, cores=cores, criterion = r.squared, minmax="max",
+                   return.models=FALSE)
+      )
+    } else {
+      res <- FSA(fitfunc=lm,formula="mpg~hp+wt", data=mtcars, fixvar="hp",
+                 quad=F, m=2, numrs=10, cores=cores, criterion = r.squared, minmax="max",
+                 return.models=FALSE)
+    }
+    
     expect_equivalent(as.formula(res$table$formula), as.formula("mpg~hp+hp*wt"))
     expect_equal(res$table$criterion, 0.8847637, tolerance=1e-3,check.attributes=F)
     expect_false("swapped.to.model" %in% names(res$solutions))
     expect_false("checked.model" %in% names(res$solutions))
 
-
     set.seed(1000)
-    res <- FSA(fitfunc=lm,formula="mpg~hp+wt", data=mtcars, fixvar="hp",
-               quad=F, m=2, numrs=10, cores=cores, criterion = r.squared, minmax="max",
-               return.models=TRUE)
+    if (.Platform$OS.type == "windows" & cores > 1) {
+      expect_warning(
+        res <- FSA(fitfunc=lm,formula="mpg~hp+wt", data=mtcars, fixvar="hp",
+                   quad=F, m=2, numrs=10, cores=cores, criterion = r.squared, minmax="max",
+                   return.models=TRUE)
+      )
+    } else {
+      res <- FSA(fitfunc=lm,formula="mpg~hp+wt", data=mtcars, fixvar="hp",
+                 quad=F, m=2, numrs=10, cores=cores, criterion = r.squared, minmax="max",
+                 return.models=TRUE)
+    }
     expect_equivalent(as.formula(res$table$formula), as.formula("mpg~hp+hp*wt"))
     expect_equal(res$table$criterion, 0.8847637, tolerance=1e-3,check.attributes=FALSE)
     expect_true("swapped.to.model" %in% names(res$solutions))
@@ -80,10 +115,17 @@ for (cores in c(1, parallel::detectCores())) {
     FSA.data <- data.frame(x,y)
 
     set.seed(1000)
-    res <- FSA(
-      y~1,data=FSA.data,fixvar=c('X1','X3'), numrs=10, cores=cores,
-      m=2, criterion=r.squared, minmax='max')
-    
+    if (.Platform$OS.type == "windows" & cores > 1) {
+      expect_warning(
+        res <- FSA(
+          y~1,data=FSA.data,fixvar=c('X1','X3'), numrs=10, cores=cores,
+          m=2, criterion=r.squared, minmax='max')
+      )
+    } else {
+      res <- FSA(
+        y~1,data=FSA.data,fixvar=c('X1','X3'), numrs=10, cores=cores,
+        m=2, criterion=r.squared, minmax='max')
+    }
     expect_equivalent(as.formula(res$table$formula[1]), as.formula("y~X1+X3+X35*X82"))
     expect_equivalent(as.formula(res$table$formula[2]), as.formula("y~X1+X3+X8*X47"))
     expect_equivalent(as.formula(res$table$formula[3]), as.formula("y~X1+X3+X9*X86"))
@@ -94,8 +136,16 @@ for (cores in c(1, parallel::detectCores())) {
 for (cores in c(1, parallel::detectCores())) {
   test_that("glm",{
     set.seed(1000)
-    res <- FSA(fitfunc=glm, formula="vs~disp+mpg", data=mtcars,
-               cores=cores,numrs=10, criterion=AIC, minmax="min")
+    if (.Platform$OS.type == "windows" & cores > 1) {
+      expect_warning(
+        res <- FSA(fitfunc=glm, formula="vs~disp+mpg", data=mtcars,
+                   cores=cores,numrs=10, criterion=AIC, minmax="min")
+      )
+    } else {
+      res <- FSA(fitfunc=glm, formula="vs~disp+mpg", data=mtcars,
+                 cores=cores,numrs=10, criterion=AIC, minmax="min")
+    }
+    
     expect_equivalent(as.formula(res$table$formula[1]), as.formula("vs~cyl*qsec"))
     expect_equal(res$table$criterion, 9.425098, tolerance=1e-5)
   })
