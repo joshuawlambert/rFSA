1efc835a7e0693501e0a65cf8ad3ea31e25beb6b
Josh L <joshua.lambert@uky.edu>
2017-12-21 12:58:17 -0500

    fixes to criterion

--- R/criterion.R
+++ R/criterion.R
@@ -83,11 +83,19 @@ int.p.val <- function(model,name = "Interaction P-Value") {
 #' @importFrom stats AIC anova as.formula cov fitted formula glm influence lm predict resid var
 #' @importFrom utils capture.output tail
 #' @export
+#' 
+#' @examples
+#' #To use Bhattacharyya Distance and FSA the response must be binary, and you must
+#' #be considering searching for two way continuous interactions. 
+#' data(mtcars)
+#' fit<-FSA(formula = "am~gear*hp",data = mtcars,
+#' fitfunc = glm,family="binomial",m = 2,cores=1,
+#' interactions = T,criterion = bdist,minmax = "max")
 bdist <- function(model,name = "B Distance") {
   if(length(grep(":",names(model$coefficients)))==0){return(0)} #no interaction in model to check
-  if(is.null(model$family)){return(0)} #not logistic regression
+  if(model$family$link!="logit"){return(0)} #not logistic regression
   
-  nam<-c(all.vars(model$formula)[1],strsplit(names(model$coefficients)[grep(":",names(model$coefficients))],"[:]")[[1]])
+  nam<-c(all.vars(as.formula(model$formula))[1],strsplit(names(model$coefficients)[grep(":",names(model$coefficients))],"[:]")[[1]])
   tmp_dat <- eval(model$model)
   y <- tmp_dat[,nam[1]]
   x1 <- tmp_dat[,nam[2]]
@@ -176,9 +184,12 @@ list.criterion<-function(){
   show("You can write your own criterion function too! Or use other criterion functions from other packages. Just follow the standard format used in int.p.val or apress as an example.")
 }
 
-#' Return QICu for rFSA
+#' Return QICu for geepack::geeglm
 #' @description Computes quasi-likelihood under the independence criterion (QICu)
 #' @param gee.obj geeglm obj
+#' @importFrom graphics par plot
+#' @importFrom stats AIC anova as.formula cov fitted formula glm influence lm predict resid var
+#' @importFrom utils capture.output tail
 #' @export
 QICu.geeglm = function(gee.obj){ 
   tryCatch(expr = {
@@ -204,4 +215,6 @@ QICu.geeglm = function(gee.obj){
   error = function(e){ print(e); return(NA) }, 
   warning = function(e){ print(e); return(NA) } )
 }
+
+
 
