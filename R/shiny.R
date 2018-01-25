44feb395041192a1f4f0be09009b986cca993940
Joshua Lambert <jmsuk0587@gmail.com>
2018-01-25 15:49:34 -0500

    Create shiny.R
    For making shinyFSA() function

---
+++ R/shiny.R
@@ -0,0 +1,22 @@
+my_server <- function(input, output) {
+  output$distPlot <- renderPlot({
+    my_plot(input$obs)
+  })
+}
+
+# Needs to be a function so nested dependencies are lazily loaded.
+my_ui <- function(){fluidPage(
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("obs", "Number of observations:", min = 10, max = 500, value = 100)
+    ),
+    mainPanel(plotOutput("distPlot"))
+  )
+)}
+
+#' @title Function \code{my_app}
+#' @description Runs the Shiny app.
+#' @export
+shinyFSA= function(){
+  shinyApp(ui = my_ui(), server = my_server)
+}
